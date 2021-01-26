ad82f697efd69ab5169849e1a2d131ba98b3bf25
Rodrigo Heredia <rodrigoh1205@gmail.com>
2021-01-26 12:07:54 -0400

    Adding router and SearchView page

--- src/router.js
+++ src/router.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import searchView from "@/views/searchView.vue";
+import home from "@/App.vue";
+import searchView from "@/views/SearchView.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
+    {
+      path: "/home",
+      name: "home",
+      component: home
+    },
     {
       path: "/search",
       name: "search",
---
+++ src/views/searchView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+        <v-container>
+            <v-layout row>
+                <v-flex xs11>
+                    <v-text-field
+                        v-model="request"
+                        filled
+                        label="Enter Artist Name"
+                        clearable
+                    >
+                    </v-text-field>
+                </v-flex>
+                <v-flex>
+                    <v-btn outlined type="submit" height="55" width="100">
+                        <v-icon>mdi-magnify</v-icon>
+                    </v-btn>
+                </v-flex>
+            </v-layout>
+        </v-container>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data() {
+    return {
+        request: 'Jack+Johnson'
+      }
+  },
+};
+</script>
